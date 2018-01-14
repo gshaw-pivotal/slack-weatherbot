832a07d1f15d9de14ff67e064e78b65aa6f6a95e
gshaw-pivotal <gshaw+github@pivotal.io>
2018-01-13 23:51:19 -0500

    Update the formatting of the uptime message

--- src/formatters/uptime_formatter.js
+++ src/formatters/uptime_formatter.js
@@ -1,17 +1,22 @@
 formatUptime = function (uptime) {
-    var unit = 'second';
-    if (uptime > 60) {
-        uptime = uptime / 60;
-        unit = 'minute';
+    var result = ''
+    var processedUptime = uptime
+
+    if (processedUptime >= 86400) {
+        result = result + Math.floor(processedUptime / 86400) + ' days'
+        processedUptime = processedUptime % 86400
     }
-    if (uptime > 60) {
-        uptime = uptime / 60;
-        unit = 'hour';
+    if (processedUptime >= 3600) {
+        result = result + ' ' + Math.floor(processedUptime / 3600) + ' hours'
+        processedUptime = processedUptime % 3600
     }
-    if (uptime != 1) {
-        unit = unit + 's';
+    if (processedUptime >= 60) {
+        result = result + ' ' + Math.floor(processedUptime / 60) + ' minutes'
+        processedUptime = processedUptime % 60
+    }
+    if (processedUptime != 0) {
+        result = result + ' ' + processedUptime + ' seconds'
     }
 
-    uptime = uptime + ' ' + unit;
-    return uptime;
-};
+    return result.trim()
+}
